1f395b3c4d187477b4312a26f4f891994fda0555
Vasya564 <dudlavasil@gmail.com>
2023-10-30 19:35:46 +0200

    Updates

---
+++ assets/dummyChart-0730f287.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-1f9911f2.js";const t=()=>r.jsxs("ul",{children:[r.jsx("li",{children:"Dummy"}),r.jsx("li",{children:"Chart"})]});export{t as default};
---
+++ assets/dummyList-b84590f7.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-1f9911f2.js";const r=()=>s.jsxs("ul",{children:[s.jsx("li",{children:"Dummy"}),s.jsx("li",{children:"List"})]});export{r as default};
